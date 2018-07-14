f815834ccf8bd3b55981bf5a209b00b26b3471f6
liao <308627057@qq.com>
2018-07-14 13:31:09 +0800

    Mod:增加userindex（用户中心）路由链接

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import Vue from "vue";
 import Router from "vue-router";
-import myproduct from "@/views/myproduct/myproduct.vue";
+import Myproduct from '@/views/myproduct/myproduct.vue';
+import Userindex from '@/views/userindex/userindex.vue'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "myproduct",
-      component: myproduct
+      path:'/',
+      name:'Myproduct',
+      component:Myproduct
+    },
+    {
+      path:'/userindex',
+      name:"Userindex",
+      component:Userindex
     }
   ]
 });
