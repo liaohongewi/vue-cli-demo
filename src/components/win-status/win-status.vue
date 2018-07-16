f45f557a225372ec737f95c3653eb2a18ec22b99
liao <308627057@qq.com>
2018-07-16 10:14:38 +0800

    Add:新增win-status.vue(用户中心状态栏)

---
+++ src/components/win-status/win-status.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="UCcenter">
+        <div class="UCcenter__router">
+            <i class="UCicon -setting"></i>
+        </div>
+
+        <div class="UCcenter__avatar">
+            <img class="UCavatar" src="@/assets/images/UserCenter/avatar.png">
+        </div>
+
+        <div class="UCcenter__router">
+            <i class="UCicon -email"></i>
+        </div>
+
+        <div class="UCcenter__helper">
+            账户管理&ensp;<i class="UCicon -router"></i>
+        </div>
+    </div>
+</template>
