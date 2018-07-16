9f162b91f7ee29d9c57b5fa5f5916d473a604c8a
liao <308627057@qq.com>
2018-07-16 14:34:50 +0800

    Mod:修改footer.vue  把格式更改成.pug

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,20 +1,15 @@
-<template>
-    <nav class="bar bar-tab">
-        <router-link class="tab-item active" to="/">
-            <span class="icon iconfont i-home"></span>
-            <span class="tab-label">首页</span>
-        </router-link>
-        <router-link class="tab-item" to="/">
-            <span class="icon iconfont i-find"></span>
-            <span class="tab-label">分类</span>
-        </router-link>
-        <router-link class="tab-item" to="/">
-            <span class="icon iconfont i-shopcart"></span>
-            <span class="tab-label">购物车</span>
-        </router-link>
-        <router-link class="tab-item" to="/userindex">
-            <span class="icon iconfont i-mine"></span>
-            <span class="tab-label">我的</span>
-        </router-link>
-    </nav>
+<template lang="pug">
+    nav.bar.bar-tab
+        router-link.tab-item(to="/")
+            span.icon.iconfont.i-home
+            span.tab-label.homefont 首页
+        router-link.tab-item(to="/")
+            span.icon.iconfont.i-find
+            span.tab-label.findfont 分类
+        router-link.tab-item(to="/")
+            span.icon.iconfont.i-shopcart
+            span.tab-label.shopcartfont 购物车
+        router-link.tab-item(to="/userindex")
+            span.icon.iconfont.i-mine
+            span.tab-label.minefont 我的
 </template>
