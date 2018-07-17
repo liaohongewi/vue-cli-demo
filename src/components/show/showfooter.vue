7f9d1aba6764ff8ed173fded75f613168456fc17
liao <308627057@qq.com>
2018-07-17 17:40:09 +0800

    Add:增加showfooter.vue(商品详情底部)

---
+++ src/components/show/showfooter.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    .box_bottom
+        a.box_btn_dianpu
+            img(src="@/assets/images/img_Commodity/shangpin-dianpu.png")  
+            |店铺
+        a.box_btn_dianpu
+            img(src="@/assets/images/img_Commodity/shoppingcart.png")
+            |购物车
+            span  1
+        a.box_btn_gouwuche 加入购物车
+        a.box_btn_goumai 立即购买
+</template>
