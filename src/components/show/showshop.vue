317c2747d8768332c22ac7949937e4530cf73bf6
liao <308627057@qq.com>
2018-07-17 17:41:07 +0800

    Add:增加showshop.vue(商品详情店铺模块)

---
+++ src/components/show/showshop.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+    .box_header_txt_info
+        p.box_txt_title 路人甲
+        p 泰禾广场
+        span.box_txt_right 211m
+        img.box_txt_phone(src="@/assets/images/img_Commodity/commodity-phone.png")
+</template>
