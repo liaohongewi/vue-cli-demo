c3fd7945deaab2e49cddff350d8a5832ed840a7c
liao <308627057@qq.com>
2018-07-17 17:39:30 +0800

    Add:增加showcontent.vue(商品详情信息)

---
+++ src/components/show/showcontent.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    .box_header_info
+        .box_header_content
+            h3 卤味大放送时间
+            .box_header_content_top
+                .box_txt_price ¥990.00
+                .box_txt_price_d ¥1000.00
+                .box_txt_chushou 已售101
+            p.box_header_content_fangxian.
+                #[span 返现] 购买返100积分 
+</template>
