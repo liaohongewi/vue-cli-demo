a0e1ad7649142ab1d5fdd3c4c0ce00e62df78964
liao <308627057@qq.com>
2018-07-17 17:41:59 +0800

    Add:增加showproduct.vue(商品详情路口页)

---
+++ src/views/showproduct/showproduct.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    .mui-content
+        .box_header
+            img.box_img_info(src="@/assets/images/img_Commodity/shoppingcart-dianpupic2.png")
+            router-link.box_img_right
+                img(src="@/assets/images/img_Commodity/commodity_img_right.png")
+            .box_info_bottom
+                h3 路人甲
+                p 随机抽取20名用户立减50
+        showdetails-nav
+        showshop-nav
+        footer-nav
+</template>
+<script>
+import details from '@/components/show/showcontent.vue'
+import shop from '@/components/show/showshop.vue'
+import footer from '@/components/show/showfooter.vue'
+export default {
+    components:{
+        "showdetails-nav":details,
+        "showshop-nav":shop,
+        "footer-nav":footer,
+    }
+}
+</script>
+
