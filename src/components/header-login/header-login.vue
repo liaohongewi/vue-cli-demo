3d5c4a9f227176894e659bf701f6906cfee18b0f
liao <308627057@qq.com>
2018-07-18 10:50:23 +0800

    Add:新增header-login.vue（用户登录头部页面）

---
+++ src/components/header-login/header-login.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+    header.bar.bar-nav.b-line
+        router-link.header-btn.header-left.iconfont.back.icon_back(to="/")
+        h1.header-title 登录
+        router-link.header-txt.header-right(to="/") 立即注册
+</template>
