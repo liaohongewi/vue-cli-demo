b4429fcd397704056d0f39c0b3fd672c2daeb52a
liao <308627057@qq.com>
2018-07-20 15:00:45 +0800

    Add:增加message-list.vue(消息中心列表组建)

---
+++ src/components/message/message-list.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    .message-list
+        p.UCtext__date 2018-02-27 14:35
+        .UCpanel.-card
+            .UCpanel__header
+                .UCpanel__title 会员升级
+            .UCpanel__main
+                p(style="width: 90%;") 会员升级！恭喜您，您已经成为 幼儿园。
+                i.UCicon.-router
+        p.UCtext__date 2018-04-1 17:35
+        .UCpanel.-card
+            .UCpanel__header
+                .UCpanel__title 会员升级
+            .UCpanel__main
+                p(style="width: 90%;") 会员升级！恭喜您，您已经成为 小学生。
+                i.UCicon.-router
+                
+</template>
