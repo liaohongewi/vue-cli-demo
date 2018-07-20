4138d8a93b775add2528f35ccc8bfaf4d152d02e
liao <308627057@qq.com>
2018-07-20 15:01:37 +0800

    Mod:修改消息中心、设置、账户管理链接

--- src/components/win-status/win-status.vue
+++ src/components/win-status/win-status.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
     .UCcenter
-        router-link.UCcenter__router(to="/login")
+        router-link.UCcenter__router(to="/setting")
             i.UCicon.-setting
         router-link.UCcenter__avatar(to="/login")
             img.UCavatar(src="@/assets/images/UserCenter/avatar.png")
-        router-link.UCcenter__router(to="/login")
+        router-link.UCcenter__router(to="/message")
             i.UCicon.-email
-        router-link.UCcenter__helper(to="/login") 账户管理&ensp;
+        router-link.UCcenter__helper(to="/account") 账户管理&ensp;
             i.UCicon.-router
 </template>
