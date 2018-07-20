a9b1528d840c249881f21356039ebd668e8eb745
liao <308627057@qq.com>
2018-07-20 15:02:52 +0800

    Add:增加message.vue(消息中心页面)

---
+++ src/views/message/message.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    .page-group
+        .page.page-index.page-current
+            header-nav
+            message-nav
+                
+</template>
+<script>
+import header from '@/components/header-login/header-message.vue'
+import message from '@/components/message/message-list.vue'
+export default {
+    components:{
+        'header-nav':header,
+        'message-nav':message
+    }
+}
+</script>
+
