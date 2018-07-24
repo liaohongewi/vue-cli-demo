4e9300f56b1a67daa1a19bb013ea357cb530d719
liao <308627057@qq.com>
2018-07-24 11:37:37 +0800

    Mod:通用顶部删除

--- src/views/shopcart/shopcart.vue
+++ src/views/shopcart/shopcart.vue
@@ -3,18 +3,15 @@
         .page.page-index.page-current
             header-nav
             shopcart-nav
-            footer-nav(v-bind:class="{'isshopcart':isNowPage}")
 </template>
 
 <script>
 import header from '@/components/header-login/header-cart.vue'
 import cart from '@/components/shopcart/cart-content.vue'
-import footer from '@/components/footer/footer.vue'
 export default {
     components:{
        'header-nav':header,
        'shopcart-nav':cart,
-       'footer-nav':footer,
     },
     data(){
         return{
