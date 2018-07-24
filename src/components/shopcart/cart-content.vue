e7ecebdd7115b30e161c8a6014ee9084d9bd3985
liao <308627057@qq.com>
2018-07-24 09:48:48 +0800

    Add:添加cart-contnet.vue(购物车内容组建)

---
+++ src/components/shopcart/cart-content.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+    
+    //-购物车--空
+    .box_shopCart_kong
+        img.box_kong_img(src="@/assets/images/img_ShopCart/shoppingcart-gouwuchekong.png")
+        p.box_kong_txt 购物车还是空的，您可以再逛逛
+    // //-购物车--结算
+    // .mui-content.box_shopCart
+    //     .mui-input-group.shopCart_bottom
+    //         .mui-input-row.mui-checkbox.mui-left
+    //             label 全选
+    //             input
+    //         a.box_btn_right 结算(1)
+    //         .box_txt_right
+    //                 p 合计：
+    //                     span ¥
+    //                     i 198
+    //                     span .00
+    //                 h5 不含运费
+</template>
