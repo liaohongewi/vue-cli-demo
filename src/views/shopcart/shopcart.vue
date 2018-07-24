67e259f7502cb2ca473edcb0f0cf528fdae6ed56
liao <308627057@qq.com>
2018-07-24 09:50:57 +0800

    Add:增加shopcart.vue(购物车)

---
+++ src/views/shopcart/shopcart.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+    .page-group
+        .page.page-index.page-current
+            header-nav
+            shopcart-nav
+            footer-nav(v-bind:class="{'isshopcart':isNowPage}")
+</template>
+
+<script>
+import header from '@/components/header-login/header-cart.vue'
+import cart from '@/components/shopcart/cart-content.vue'
+import footer from '@/components/footer/footer.vue'
+export default {
+    components:{
+       'header-nav':header,
+       'shopcart-nav':cart,
+       'footer-nav':footer,
+    },
+    data(){
+        return{
+            isNowPage:true
+        }
+    }
+}
+</script>
+ 
+ 
